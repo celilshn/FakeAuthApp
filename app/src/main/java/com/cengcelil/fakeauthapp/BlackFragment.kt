eaf7d597c7bbd4943bc9a6b3772b01d4db0ee7a5
celilshn <cengcelil@gmail.com>
2022-06-30 17:01:49 +0300

    packagename refactor

--- app/src/main/java/com/cengcelil/fakeauthapp/BlackFragment.kt
+++ app/src/main/java/com/cengcelil/fakeauthapp/BlackFragment.kt
@@ -1,5 +1,6 @@
 package com.cengcelil.fakeauthapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -32,6 +33,9 @@ class BlackFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
+        b.button2.setOnClickListener {
+            startActivity(Intent(requireActivity(),MainActivity2::class.java))
 
+        }
+    }
     }
